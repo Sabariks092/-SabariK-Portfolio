bb613b1eb66bc895187400a811d4e8d0576487aa
Sabariks092 <sabariks092@gmail.com>
2024-11-02 18:20:44 +0530

    removed animation and added new resume and add my own project and made some few more changes (retry 5) - commit

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -49,36 +49,36 @@ const Desc = styled.div`
   }
 `;
 
-// const ToggleButtonGroup = styled.div`
-// display: flex;
-// border: 1.5px solid ${({ theme }) => theme.primary};
-// color: ${({ theme }) => theme.primary};
-// font-size: 16px;
-// border-radius: 12px;
-// font-weight 500;
-// margin: 22px 0;
-// @media (max-width: 768px){
-//     font-size: 12px;
-// }
-// `;
+const ToggleButtonGroup = styled.div`
+display: flex;
+border: 1.5px solid ${({ theme }) => theme.primary};
+color: ${({ theme }) => theme.primary};
+font-size: 16px;
+border-radius: 12px;
+font-weight 500;
+margin: 22px 0;
+@media (max-width: 768px){
+    font-size: 12px;
+}
+`;
 
-// const ToggleButton = styled.div`
-//   padding: 8px 18px;
-//   border-radius: 6px;
-//   cursor: pointer;
-//   &:hover {
-//     background: ${({ theme }) => theme.primary + 20};
-//   }
-//   @media (max-width: 768px) {
-//     padding: 6px 8px;
-//     border-radius: 4px;
-//   }
-//   ${({ active, theme }) =>
-//     active &&
-//     `
-//   background:  ${theme.primary + 20};
-//   `}
-// `;
+const ToggleButton = styled.div`
+  padding: 8px 18px;
+  border-radius: 6px;
+  cursor: pointer;
+  &:hover {
+    background: ${({ theme }) => theme.primary + 20};
+  }
+  @media (max-width: 768px) {
+    padding: 6px 8px;
+    border-radius: 4px;
+  }
+  ${({ active, theme }) =>
+    active &&
+    `
+  background:  ${theme.primary + 20};
+  `}
+`;
 
 // const Divider = styled.div`
 //   width: 1.5px;
@@ -106,14 +106,14 @@ const Projects = ({ openModal, setOpenModal }) => {
         >
           I have worked on a wide range of projects in web development, Here are some of my projects.
         </Desc>
-        {/* <ToggleButtonGroup>
+        <ToggleButtonGroup>
           <ToggleButton
             active={toggle === "all"}
             onClick={() => setToggle("all")}
           >
             ALL
           </ToggleButton>
-          <Divider />
+          {/* <Divider />
           <ToggleButton
             active={toggle === "web app"}
             onClick={() => setToggle("web app")}
@@ -133,17 +133,17 @@ const Projects = ({ openModal, setOpenModal }) => {
             onClick={() => setToggle("machine learning")}
           >
             MACHINE LEARNING
-          </ToggleButton>
-        </ToggleButtonGroup> */}
+          </ToggleButton> */}
+        </ToggleButtonGroup>
         <CardContainer>
-          {/* {toggle === "all" &&
+          {toggle === "all" &&
             projects.map((project) => (
               <ProjectCard
                 project={project}
                 openModal={openModal}
                 setOpenModal={setOpenModal}
               />
-            ))} */}
+            ))}
           {projects
             .filter((item) => item.category === toggle)
             .map((project) => (
