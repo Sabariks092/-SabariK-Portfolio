4212a174dfb1a4d9adaa12b301caaef76a0c5e17
Sabariks092 <sabariks092@gmail.com>
2025-02-06 13:02:40 +0530

    image EXTENSION Changed

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { Bio } from "../../data/constants";
 import Typewriter from "typewriter-effect";
-import HeroImg from "../../images/kabilan.jpeg";
+import HeroImg from "../../images/kabilan.JPG";
 // import HeroBgAnimation from "../HeroBgAnimation";
 import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
